8a31d7058a9a871a62266391b35448e59138df32
Madhusudan Tripathy <madhusudantripathy77@gmail.com>
2023-02-01 17:19:33 +0530

    First Attempt

--- src/main/java/com/driver/BookController.java
+++ src/main/java/com/driver/BookController.java
@@ -47,28 +47,89 @@ public class BookController {
     @PostMapping("/create-book")
     public ResponseEntity<Book> createBook(@RequestBody Book book){
         // Your code goes here.
+        bookList.add(book);
         return new ResponseEntity<>(book, HttpStatus.CREATED);
     }
 
     // get request /get-book-by-id/{id}
     // pass id as path variable
     // getBookById()
+    @GetMapping("/get-book-by-id/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable int id){
+        for(Book book : bookList){
+            if(book.getId()==id) return new ResponseEntity<>(book,HttpStatus.OK);
+        }
+        return null;
+    }
+
 
     // delete request /delete-book-by-id/{id}
     // pass id as path variable
     // deleteBookById()
+    @DeleteMapping("/delete-book-by-id/{id}")
+    public ResponseEntity<String> deleteBookById(@PathVariable int id){
+        String message="Book Not Available";
+        for(Book book : bookList) {
+            if (book.getId() == id) {
+                bookList.remove(book);
+                message="Book With Id "+id+" De;eted Successfully";
+                break;
+            }
+        }
+        return new ResponseEntity<>(message,HttpStatus.OK);
+    }
 
     // get request /get-all-books
     // getAllBooks()
+    @GetMapping("/get-all-books")
+    public ResponseEntity<List<Book>> getAllBooks(){
+        return new ResponseEntity<>(bookList,HttpStatus.OK);
+    }
 
     // delete request /delete-all-books
     // deleteAllBooks()
+    @DeleteMapping("/delete-all-books")
+    public ResponseEntity<String> deleteAllBooks(){
+        bookList.clear();
+        return new ResponseEntity<>("All Books Removed",HttpStatus.OK);
+    }
 
     // get request /get-books-by-author
     // pass author name as request param
     // getBooksByAuthor()
+    @GetMapping("/get-books-by-author")
+    public ResponseEntity<List<Book>> getBooksByAuthor(@RequestParam String author){
+        List<Book> ls= new ArrayList<>();
+        for(Book book : bookList){
+            if(book.getAuthor().equals(author)) ls.add(book);
+        }
+        return new ResponseEntity<>(ls,HttpStatus.OK);
+    }
 
     // get request /get-books-by-genre
     // pass genre name as request param
     // getBooksByGenre()
+    @GetMapping("/get-books-by-genre")
+    public ResponseEntity<List<Book>> getBooksByGenre(@RequestParam String genre){
+        List<Book> ls= new ArrayList<>();
+        for(Book book : bookList){
+            if(book.getGenre().equals(genre)) ls.add(book);
+        }
+        return new ResponseEntity<>(ls,HttpStatus.OK);
+    }
 }
+/*
+
+Make an MVC architecture, with Book as your only model class, which will have id, name, author and genre as properties. Then make a controller class with appropriate annotations. Since you want to keep it simple, you don’t need to use any database. You can use a List of Books in the controller layer itself to store the records.
+
+You need to implement the logic for following functionalities:
+
+Save a book: /books/create-book - Return created book object as response
+Get book object by Id: /books/get-book-by-id/{id} - Return book object as response
+Get all books: /books/get-all-books - Return book object as response
+Get books by author: /books/get-books-by-author?author=author+name
+Get books by genre: /books/get-books-by-genre?genre=genre+name
+Delete book by id: /books/delete-book-by-id/{id}
+Delete all books: /books/delete-all-books\
+
+*/
